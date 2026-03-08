50e32e3e02f8c15c1d1eb8e84ddf9e8afb1aff65
bluefloyd24 <gluhdb@gmail.com>
2026-03-09 05:40:29 +0700

    Implement KeyboardButtonStyleBgPrimary class
    Add KeyboardButtonStyleBgPrimary class for button styling.

--- pyrogram/raw/types/keyboard_button_style_bg_primary.py
+++ pyrogram/raw/types/keyboard_button_style_bg_primary.py
@@ -1,1 +1,21 @@
+from ..object import Object
 
+
+class KeyboardButtonStyleBgPrimary(Object):
+    """Blue/primary background style for keyboard buttons.
+    
+    Constructor: keyboardButtonStyleBgPrimary#a3c61f72
+    """
+
+    ID = 0xa3c61f72
+    QUALNAME = "types.KeyboardButtonStyleBgPrimary"
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def read(b: "BytesIO", *args) -> "KeyboardButtonStyleBgPrimary":
+        return KeyboardButtonStyleBgPrimary()
+
+    def write(self, *args) -> bytes:
+        return self.ID.to_bytes(4, "little")
