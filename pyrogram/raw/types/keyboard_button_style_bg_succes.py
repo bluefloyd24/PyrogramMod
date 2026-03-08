a19eac1cf7f0148c1b447d846026af0076c65116
bluefloyd24 <gluhdb@gmail.com>
2026-03-09 05:41:06 +0700

    Implement KeyboardButtonStyleBgSuccess class
    Added a new class for green/success background style for keyboard buttons.

--- pyrogram/raw/types/keyboard_button_style_bg_succes.py
+++ pyrogram/raw/types/keyboard_button_style_bg_succes.py
@@ -1,1 +1,21 @@
+from ..object import Object
 
+
+class KeyboardButtonStyleBgSuccess(Object):
+    """Green/success background style for keyboard buttons.
+    
+    Constructor: keyboardButtonStyleBgSuccess#8a0d5de7
+    """
+
+    ID = 0x8a0d5de7
+    QUALNAME = "types.KeyboardButtonStyleBgSuccess"
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def read(b: "BytesIO", *args) -> "KeyboardButtonStyleBgSuccess":
+        return KeyboardButtonStyleBgSuccess()
+
+    def write(self, *args) -> bytes:
+        return self.ID.to_bytes(4, "little")
