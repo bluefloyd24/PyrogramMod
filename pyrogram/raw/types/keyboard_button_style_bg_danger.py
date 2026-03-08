f2decc8c256be064cdb2e691e80bb5780cc69f2e
bluefloyd24 <gluhdb@gmail.com>
2026-03-09 05:40:51 +0700

    Implement KeyboardButtonStyleBgDanger class
    Added a class for red/danger background style for keyboard buttons.

--- pyrogram/raw/types/keyboard_button_style_bg_danger.py
+++ pyrogram/raw/types/keyboard_button_style_bg_danger.py
@@ -1,1 +1,21 @@
+from ..object import Object
 
+
+class KeyboardButtonStyleBgDanger(Object):
+    """Red/danger background style for keyboard buttons.
+    
+    Constructor: keyboardButtonStyleBgDanger#99700f13
+    """
+
+    ID = 0x99700f13
+    QUALNAME = "types.KeyboardButtonStyleBgDanger"
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def read(b: "BytesIO", *args) -> "KeyboardButtonStyleBgDanger":
+        return KeyboardButtonStyleBgDanger()
+
+    def write(self, *args) -> bytes:
+        return self.ID.to_bytes(4, "little")
