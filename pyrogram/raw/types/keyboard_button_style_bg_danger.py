baa511ff1af3f9f8f1bfb89805106e086770b34a
bluefloyd24 <gluhdb@gmail.com>
2026-03-09 12:16:30 +0700

    Update keyboard_button_style_bg_danger.py

--- pyrogram/raw/types/keyboard_button_style_bg_danger.py
+++ pyrogram/raw/types/keyboard_button_style_bg_danger.py
@@ -1,21 +1,25 @@
-from ..object import Object
+from io import BytesIO
+from typing import Any
+from pyrogram.raw.core.primitives import Int
+from pyrogram.raw.core import TLObject
 
 
-class KeyboardButtonStyleBgDanger(Object):
-    """Red/danger background style for keyboard buttons.
-    
-    Constructor: keyboardButtonStyleBgDanger#99700f13
-    """
+class KeyboardButtonStyleBgDanger(TLObject):  # type: ignore
+    """Red/danger background style for keyboard buttons."""
+
+    __slots__ = []
 
     ID = 0x99700f13
     QUALNAME = "types.KeyboardButtonStyleBgDanger"
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self) -> None:
+        pass
 
     @staticmethod
-    def read(b: "BytesIO", *args) -> "KeyboardButtonStyleBgDanger":
+    def read(b: BytesIO, *args: Any) -> "KeyboardButtonStyleBgDanger":
         return KeyboardButtonStyleBgDanger()
 
     def write(self, *args) -> bytes:
-        return self.ID.to_bytes(4, "little")
+        b = BytesIO()
+        b.write(Int(self.ID, False))
+        return b.getvalue()
